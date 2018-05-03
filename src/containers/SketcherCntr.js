dd4ed62eca66200f6760502fe52eca4a0975f96d
Jemaine Brown <jembrown@Jemaines-MacBook-Pro.local>
2018-05-03 00:06:37 -0500

    Can paint in the canvas and change the brush size.

--- src/containers/SketcherCntr.js
+++ src/containers/SketcherCntr.js
@@ -10,12 +10,14 @@ class SketcherCntr extends Component {
       colorPicker: false,
       brushRadius: 5,
       eraserRadius: 5,
+      dragging: false
     }
   }
 
   componentDidMount () {
     // A reference to the canvas
     const canvas = this.refs.canvas;
+    console.log(canvas.getContext('2d'));
 
     // The size of the canvas
     canvas.width = 400;
@@ -34,7 +36,15 @@ class SketcherCntr extends Component {
     });
   }
 
-  renderBrush = (e) => {
+  engage = (e) => {
+    this.setState({ dragging: true });
+
+    // A point is drawn without dragging the mouse
+    this.putPoint(e, false);
+  }
+
+  // The click parameter is needed so a point is drawn without dragging the mouse
+  putPoint = (e, click) => {
     const { brushRadius } = this.state;
 
     // A reference to the canvas to get the context
@@ -43,17 +53,72 @@ class SketcherCntr extends Component {
     // Mouse position
     const x = e.nativeEvent.offsetX,
           y = e.nativeEvent.offsetY;
-
-    // The beginPath method beigns a path and resets the current path
-    context.beginPath();
     
-    // context.arc(x, y, radius, startAngle, endAngle, [antiClockwise]);
-    context.arc(x, y, brushRadius, 0, 2 * Math.PI);
+    if ( this.state.dragging || click ) {
+      context.lineWidth = brushRadius * 2;
+      context.lineCap = 'round';
 
-    // Stroke color
-    // context.strokeStyle = 'slateblue';
-    context.stroke();
+      // 
+      context.lineTo(x, y);
+
+      // The stroke method draws the path defined by lineTo and moveTo
+      context.strokeStyle = 'slateblue';
+      context.stroke();
+      
+      // A circle is created using the arc method. The start and end angles make the arc a circle. 2*PI is one cycle around a circle in radians. 
+      context.arc(x, y, brushRadius, 0, 2 * Math.PI);  // context.arc(x, y, radius, startAngle, endAngle, [antiClockwise]);
+
+      // Fills the circle with a color (without fillStyle the color is black by default)
+      context.fillStyle = 'slateblue';
+      context.fill();
+
+      // The path is reset
+      context.beginPath();
+
+      // 
+      context.moveTo(x, y)
+    }
   }
+
+  // If the mouse is out the canvas the path will disconnect
+  resetPath = () => {
+    const context = this.refs.canvas.getContext('2d');
+    context.beginPath();
+  }
+
+  disengage = () => {
+    this.setState({ dragging: false });
+
+    // The path is reset. All the paths would be connected without a reseting when the mouse is up.
+    const context = this.refs.canvas.getContext('2d');
+    context.beginPath();
+  }
+
+  // renderBrush = (e) => {
+  //   // this.resetPath();
+  //   const { brushRadius } = this.state;
+
+  //   // A reference to the canvas to get the context
+  //   const context = this.refs.canvas.getContext('2d');
+    
+  //   // Mouse position
+  //   const x = e.nativeEvent.offsetX,
+  //         y = e.nativeEvent.offsetY;
+    
+  //   // Stroke color
+  //   context.strokeStyle = 'white';
+  //   context.stroke();
+
+  //   // The beginPath method beigns a path and resets the current path
+  //   context.beginPath();
+    
+  //   // context.arc(x, y, radius, startAngle, endAngle, [antiClockwise]);
+  //   context.arc(x, y, brushRadius, 0, 2 * Math.PI);
+
+  //   // Stroke color
+  //   context.strokeStyle = 'blue';
+  //   context.stroke();
+  // }
 
   render() {
     const { brush, eraser, colorPicker, brushRadius, eraserRadius } = this.state;
@@ -66,7 +131,12 @@ class SketcherCntr extends Component {
                 eraserRadius={ parseInt(eraserRadius, 10) }
                 handleChange={ this.handleChange }
                 handleToolChange={ this.handleToolChange }>
-        <canvas ref="canvas" onMouseMove={ this.renderBrush }/>
+
+        <canvas ref="canvas" onMouseDown={ this.engage } 
+                             onMouseMove={ (e) => this.putPoint(e, false) }
+                            //  onMouseMove={ this.renderBrush }
+                             onMouseOut={ this.resetPath }
+                             onMouseUp={ this.disengage }/>
       </Sketcher>
     );
   }
